51b97aa7c8d94291b57a84127ba9819134ff82b4
Ruslan Kazankov <kazankovruslankrk@gmail.com>
2025-07-30 15:20:50 +0800

    Add integration tests on api/platforms/upload and api/platforms/search requests.

--- Src/EffectiveMobile.AdPlatforms.Tests/IntegrationTests/PlatformsUploadTests.cs
+++ Src/EffectiveMobile.AdPlatforms.Tests/IntegrationTests/PlatformsUploadTests.cs
@@ -1,5 +1,4 @@
 using EffectiveMobile.AdPlatforms.Api;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Xunit.Abstractions;
 
@@ -8,38 +7,156 @@ namespace EffectiveMobile.AdPlatforms.Tests.IntegrationTests;
 public class PlatformsUploadTests : IClassFixture<WebApplicationFactory<Program>>
 {
     private readonly WebApplicationFactory<Program> _factory;
-    private readonly ITestOutputHelper _testOutputHelper;
     private const string RequestUri = "api/platforms/upload";
+    private const string UploadFileParameter = "locations";
 
-    public PlatformsUploadTests(WebApplicationFactory<Program> factory, ITestOutputHelper testOutputHelper)
+    public PlatformsUploadTests(WebApplicationFactory<Program> factory)
     {
         _factory = factory;
-        _testOutputHelper = testOutputHelper;
     }
     
     [Fact]
-    public async Task Upload_SuccessTest()
+    public async Task Upload_DefaultFileSuccessTest()
     {
         // Arrange
         var client = _factory.CreateClient();
-        var content = """
-                      Яндекс.Директ:/ru
-                      Ревдинский рабочий:/ru/svrd/revda,/ru/svrd/pervik
-                      Газета уральских москвичей:/ru/msk,/ru/permobl,/ru/chelobl
-                      Крутая реклама:/ru/svrd
-                      """;
-        var filename = "test.txt";
-        var filebytes = System.Text.Encoding.UTF8.GetBytes(content);
-        var byteContent = new ByteArrayContent(filebytes);
+        const string content = """
+                               Яндекс.Директ:/ru
+                               Ревдинский рабочий:/ru/svrd/revda,/ru/svrd/pervik
+                               Газета уральских москвичей:/ru/msk,/ru/permobl,/ru/chelobl
+                               Крутая реклама:/ru/svrd
+                               """;
+        const string filename = "test.txt";
+        var fileBytes = System.Text.Encoding.UTF8.GetBytes(content);
+        var byteContent = new ByteArrayContent(fileBytes);
         
         var form = new MultipartFormDataContent();
-        form.Add(byteContent, "locations", filename);
+        form.Add(byteContent, UploadFileParameter, filename);
         
         // Act
         var response = await client.PostAsync(RequestUri, form);
         
         // Assert
-        _testOutputHelper.WriteLine(await response.Content.ReadAsStringAsync());
         Assert.True(response.IsSuccessStatusCode);
+    }
+    
+    [Fact]
+    public async Task Upload_EmptyFileSuccessTest()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+        var content = string.Empty;
+        const string filename = "test.txt";
+        var fileBytes = System.Text.Encoding.UTF8.GetBytes(content);
+        var byteContent = new ByteArrayContent(fileBytes);
+        
+        var form = new MultipartFormDataContent();
+        form.Add(byteContent, UploadFileParameter, filename);
+        
+        // Act
+        var response = await client.PostAsync(RequestUri, form);
+        
+        // Assert
+        Assert.True(response.IsSuccessStatusCode);
+    }
+
+    [Fact]
+    public async Task Upload_MissingSplitSymbolFailureTest()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+        var content = "text without split symbol"; // Without ':'
+        const string filename = "test.txt";
+        var fileBytes = System.Text.Encoding.UTF8.GetBytes(content);
+        var byteContent = new ByteArrayContent(fileBytes);
+        
+        var form = new MultipartFormDataContent();
+        form.Add(byteContent, UploadFileParameter, filename);
+        
+        // Act
+        var response = await client.PostAsync(RequestUri, form);
+        
+        // Assert
+        Assert.False(response.IsSuccessStatusCode);
+    }
+    
+    [Fact]
+    public async Task Upload_MissingLocationsFailureTest()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+        var content = "MissingLocations:";
+        const string filename = "test.txt";
+        var fileBytes = System.Text.Encoding.UTF8.GetBytes(content);
+        var byteContent = new ByteArrayContent(fileBytes);
+        
+        var form = new MultipartFormDataContent();
+        form.Add(byteContent, UploadFileParameter, filename);
+        
+        // Act
+        var response = await client.PostAsync(RequestUri, form);
+        
+        // Assert
+        Assert.False(response.IsSuccessStatusCode);
+    }
+    
+    [Fact]
+    public async Task Upload_MissingPlatformFailureTest()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+        const string content = ":MissingPlatform";
+        const string filename = "test.txt";
+        var fileBytes = System.Text.Encoding.UTF8.GetBytes(content);
+        var byteContent = new ByteArrayContent(fileBytes);
+        
+        var form = new MultipartFormDataContent();
+        form.Add(byteContent, UploadFileParameter, filename);
+        
+        // Act
+        var response = await client.PostAsync(RequestUri, form);
+        
+        // Assert
+        Assert.False(response.IsSuccessStatusCode);
+    }
+    
+    [Fact]
+    public async Task Upload_MissingSlashSymbolFailureTest()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+        const string content = "testLocation:testPlatform";
+        const string filename = "test.txt";
+        var fileBytes = System.Text.Encoding.UTF8.GetBytes(content);
+        var byteContent = new ByteArrayContent(fileBytes);
+        
+        var form = new MultipartFormDataContent();
+        form.Add(byteContent, UploadFileParameter, filename);
+        
+        // Act
+        var response = await client.PostAsync(RequestUri, form);
+        
+        // Assert
+        Assert.False(response.IsSuccessStatusCode);
+    }
+    
+    [Fact]
+    public async Task Upload_ExtraSplitSymbolFailureTest()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+        const string content = "testLocation:test:Platform";
+        const string filename = "test.txt";
+        var fileBytes = System.Text.Encoding.UTF8.GetBytes(content);
+        var byteContent = new ByteArrayContent(fileBytes);
+        
+        var form = new MultipartFormDataContent();
+        form.Add(byteContent, UploadFileParameter, filename);
+        
+        // Act
+        var response = await client.PostAsync(RequestUri, form);
+        
+        // Assert
+        Assert.False(response.IsSuccessStatusCode);
     }
 }
